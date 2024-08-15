93d0c98d18d19572e683c3bb23a2f18a84cf5181
KaiRenken <kai.renken@gmx.de>
2024-08-15 10:12:56 +0200

    add temperature chart

--- src/main/react/src/App.tsx
+++ src/main/react/src/App.tsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import './App.css'
 import {LineChart} from "@mui/x-charts";
 import {HttpClientResponse, httpGet} from "./utils/HttpClient.ts";
+import dayjs from "dayjs";
 
 type MomentaufnahmeDto = {
     id?: string,
@@ -21,29 +22,56 @@ type MomentaufnahmeDto = {
 
 function App() {
     const [momentaufnahmen, setMomentaufnahmen] = useState<MomentaufnahmeDto[]>([]);
+    const [isLoading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => getMomentaufnahmen());
+    useEffect(() => {
+        getMomentaufnahmen()
+    }, []);
 
     const getMomentaufnahmen = () => {
-        let path = "/api/momentaufnahmen/?from=2024-08-13T15:19&to=2024-08-13T15:57";
+        let path = "/api/momentaufnahme/?from=2024-08-13T15:19&to=2024-08-13T15:57";
         httpGet<MomentaufnahmeDto[]>(path)
-            .then((response: HttpClientResponse<MomentaufnahmeDto[]>) => setMomentaufnahmen(response.data))
+            .then((response: HttpClientResponse<MomentaufnahmeDto[]>) => {
+                setMomentaufnahmen(response.data)
+            })
+            .finally(() => setLoading(false));
+    }
+
+    const renderContent = () => {
+        if (isLoading) return <div>Lädt Daten...</div>
+
+        const xAxisData = momentaufnahmen.map(momentaufnahme => new Date(momentaufnahme.zeitpunkt))
+
+        const yAxisData = momentaufnahmen.map(momentaufnahme => momentaufnahme.temperatur ?? null)
+
+        return (
+            <div>
+                <LineChart
+                    xAxis={[
+                        {
+                            label: "Date",
+                            data: xAxisData,
+                            tickInterval: "auto",
+                            scaleType: "time",
+                            valueFormatter: (date) => dayjs(date).format("DD/MM/YYYY H:mm")
+                        },
+                    ]}
+                    yAxis={[{label: "Temperatur (°C)"}]}
+                    series={[
+                        {
+                            data: yAxisData
+                        },
+                    ]}
+                    height={1000}
+                    width={1500}
+                />
+            </div>
+        )
     }
 
     return (
         <>
-            <div>
-                <LineChart
-                    xAxis={[{data: [momentaufnahmen[0].zeitpunkt, momentaufnahmen[1].zeitpunkt, momentaufnahmen[2].zeitpunkt]}]}
-                    series={[
-                        {
-                            data: [momentaufnahmen[0].temperatur, momentaufnahmen[1].temperatur, momentaufnahmen[2].temperatur],
-                        },
-                    ]}
-                    width={500}
-                    height={300}
-                />
-            </div>
+            {renderContent()}
         </>
     )
 }

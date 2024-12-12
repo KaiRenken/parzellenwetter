8fad3cefd5f7fa4516a18a6ff95149b1074527a1
KaiRenken <kai.renken@gmx.de>
2024-12-12 21:14:55 +0100

    Add empty test cases and todos

---
+++ src/test/kotlin/de/kairenken/parzellenwetter/infrastructure/wetter/client/WetterClientTest.kt
@@ -0,0 +1,18 @@
+package de.kairenken.parzellenwetter.infrastructure.wetter.client
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("Fetch Wetter data")
+internal class WetterClientTest {
+
+    @Test
+    fun successfully() {
+        TODO("Implement me!")
+    }
+
+    @Test
+    fun `with server not available`() {
+        TODO("Implement me!")
+    }
+}
---
+++ src/test/kotlin/de/kairenken/parzellenwetter/infrastructure/wetter/rest/WetterControllerTest.kt
@@ -0,0 +1,23 @@
+package de.kairenken.parzellenwetter.infrastructure.wetter.rest
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class WetterControllerTest {
+
+    @Nested
+    @DisplayName("Hole Wetter")
+    inner class HoleWetterTest {
+
+        @Test
+        fun erfolgreich() {
+            TODO("Implement me!")
+        }
+
+        @Test
+        fun `ohne Daten`() {
+            TODO("Implement me!")
+        }
+    }
+}
